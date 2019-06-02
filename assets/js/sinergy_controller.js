c85c2f60c360c8a9876e270266cefafe9847bce8
Carlo Gilmar <carlogilmar12@gmail.com>
2019-06-02 03:38:05 -0500

    Storing all values

--- assets/js/sinergy_controller.js
+++ assets/js/sinergy_controller.js
@@ -16,7 +16,8 @@ export const app = new Vue({
 		service_time: 0,
 		servers: 0,
 		external_flow: 0,
-		stations_in_network: []
+		stations_in_network: [],
+		station_for_store: null
 	},
 	created: function() {
 		console.log("Trying to connect socket implementation...");
@@ -42,8 +43,23 @@ export const app = new Vue({
 			}
 		},
 		store_station: function(){
-
-		alert("Guardando!");
+			this.station_for_store = new Map();
+			this.station_for_store.set("service_time", this.service_time);
+			this.station_for_store.set("servers", this.servers);
+			this.station_for_store.set("external_flow", this.external_flow);
+			this.station_for_store.set("process_route", this.process_route_values);
+			this.stations_in_network.push(this.station_for_store)
+			alert("Agregada");
+			if(this.current_station == this.stations){
+				alert("Se acabó");
+			} else {
+				alert("Continuamosss");
+				this.current_station = this.current_station+1;
+				this.service_time = 0;
+				this.servers = 0;
+				this.external_flow = 0;
+				for (this.i = 0; this.i < this.stations; this.i++){ this.process_route_values[this.i] = 0; }
+			}
 		}
 	}
 });
